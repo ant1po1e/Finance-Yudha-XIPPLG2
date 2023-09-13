d1074aeee8b12816d9ae9e1648344ddd795d5638
ant1po1e <symamaxim@gmail.com>
2023-09-13 16:25:11 +0700

    fixed responsive

--- dist/js/script.js
+++ dist/js/script.js
@@ -70,44 +70,44 @@ function sleep(ms) {
    return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-const phrases = [
-   "Make a Payment",
-   "Transfer",
-   "Top-up" 
-];
-const el = document.getElementById("typewriter");
+// const phrases = [
+//    "Make a Payment",
+//    "Transfer",
+//    "Top-up" 
+// ];
+// const el = document.getElementById("typewriter");
 
-let sleepTime = 100;
+// let sleepTime = 100;
 
-let curPhraseIndex = 0;
+// let curPhraseIndex = 0;
 
-const writeLoop = async () => {
-   while (true) {
-      let curWord = phrases[curPhraseIndex];
+// const writeLoop = async () => {
+//    while (true) {
+//       let curWord = phrases[curPhraseIndex];
 
-      for (let i = 0; i < curWord.length; i++) {
-         el.innerText = curWord.substring(0, i + 1);
-         await sleep(sleepTime);
-      }
+//       for (let i = 0; i < curWord.length; i++) {
+//          el.innerText = curWord.substring(0, i + 1);
+//          await sleep(sleepTime);
+//       }
 
-      await sleep(sleepTime * 10);
+//       await sleep(sleepTime * 10);
 
-      for (let i = curWord.length; i > 0; i--) {
-         el.innerText = curWord.substring(0, i - 1);
-         await sleep(sleepTime);
-      }
+//       for (let i = curWord.length; i > 0; i--) {
+//          el.innerText = curWord.substring(0, i - 1);
+//          await sleep(sleepTime);
+//       }
 
-      await sleep(sleepTime * 5);
+//       await sleep(sleepTime * 5);
 
-      if (curPhraseIndex === phrases.length - 1) {
-         curPhraseIndex = 0;
-      } else {
-         curPhraseIndex++;
-      }
-   }
-};
+//       if (curPhraseIndex === phrases.length - 1) {
+//          curPhraseIndex = 0;
+//       } else {
+//          curPhraseIndex++;
+//       }
+//    }
+// };
 
-writeLoop();
+// writeLoop();
 
 const loader = document.getElementById("preloader");
 const body = document.body;
